efb3ad2f35b7da067b26b6d183abdb5bea49b5ab
DaveHyperspace <dave@hyperspace.us>
2023-05-16 19:23:25 +0100

    update urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,14 +11,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hyperspace.foundation/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'hyperspace', // Usually your GitHub org/user name.
   projectName: 'hyperspace-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -41,15 +41,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/DaveHyperspace/docusaurus-demo/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/DaveHyperspace/docusaurus-demo/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -85,7 +83,7 @@ const config = {
           { to: '/blog', label: 'Blog', position: 'left' },
           { to: 'showcase', label: 'Showcase', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/DaveHyperspace/docusaurus-demo',
             label: 'GitHub',
             position: 'right',
           },
